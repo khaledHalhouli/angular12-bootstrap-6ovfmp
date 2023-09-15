629716a4b17bb508e778f4848941b98b2876a9c0
khaledHalhouli <101004067+khaledHalhouli@users.noreply.github.com>
2023-09-15 19:17:47 +0300

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,13 +10,9 @@ export class AppComponent implements OnInit {
   constructor(
     private modalService: NgbModal,
     @Inject(DOCUMENT) document: Document
-  ) {
-
-  }
+  ) {}
   ngOnInit() {
-    
     this.loopFunction();
-    
   }
   public open(modal: any): void {
     this.modalService.open(modal);
@@ -24,10 +20,8 @@ export class AppComponent implements OnInit {
   row = 0;
   col = 12;
 
-
   loopFunction() {
     let array = [
-      
       {
         id: 0,
         cols: 6,
@@ -64,7 +58,7 @@ export class AppComponent implements OnInit {
         color: 'lightblue',
         index: 2,
       },
-    
+
       {
         id: 2,
         cols: 12,
@@ -83,44 +77,72 @@ export class AppComponent implements OnInit {
       this.createElement(ele);
     });
   }
-x:any=[]
+  x: any = [];
   createElement(item) {
-
-    console.log(this.row,this.col,item.rows,item.cols)
-    if (this.row < item.rows && this.col - item.cols > 0) {
+    console.log(this.x);
+    if (!this.x.length || this.x[this.x.length - 1].colSize > item.cols) {
       this.row = item.rows;
-      this.col=this.col-item.cols
+      this.col = this.col - item.cols;
 
-      console.log(' create row1& create div1->class col-item.col',item.cols,item.index);
-      let obj={row:{
-        size:item.rows,
-        colSize:(12*item.rows-item.cols*item.rows),
-        cols:[{rowSize:item.rows,colSize:item.cols}]
-      }}
-      this.x.push(obj)
+      console.log(
+        ' create row1& create div1->class col-item.col',
+        item.cols,
+        item.index
+      );
+      let obj = {
+        row: {
+          size: item.rows,
+          colSize: 12 - item.cols,
+          cols: [{ rowSize: item.rows, colSize: item.cols }],
+        },
+      };
+      this.x.push(obj);
       // create row1& create div1->class col-item.col
-      return
+      return;
     } else if (item.rows != 1) {
       // call it self
     }
-    this.x.row?.cols.forEach((ele)=>{
-
-    })
-    if (this.col - item.cols > 0) {
-      this.col = this.col - item.cols;
-      console.log('create in row1-> div2->class col-item.col',item.cols,item.index);
-      // create in row1-> div2->class col-item.col
-    } else if (this.col - item.cols == 0) {
-      console.log(' create in row1-> div2->class col-item.col',item.cols,item.index);
-      // create in row1-> div2->class col-item.col
-      this.row = this.row - 1;
-      this.col = 12;
-    } else if (this.col - item.cols < 0) {
-      this.row = item.rows;
-      this.col = 12 - item.cols;
-      console.log('create row2&create div-class col-item.col',item.cols,item.index);
-      // create row2&create div-class col-item.col
-    }
+    console.log(231);
+    // for (let i = 0; i < this.x[this.x.length - 1].row?.cols.length; i++) {
+    //   console.log(1);
+    //   let ele = this.x[this.x.length - 1].row?.cols[i];
+    //   if (ele.colSize - item.cols > 0) {
+    //     this.col = this.col - item.cols;
+    //     console.log(
+    //       'create in row1-> div2->class col-item.col',
+    //       item.cols,
+    //       item.index
+    //     );
+    //     // create in row1-> div2->class col-item.col
+    //     this.x[this.x.length - 1].row?.cols.push({
+    //       rowSize: item.rows,
+    //       colSize: item.cols,
+    //     });
+    //     break;
+    //   } else if (ele.colSize - item.cols == 0) {
+    //     console.log(
+    //       ' create in row1-> div2->class col-item.col',
+    //       item.cols,
+    //       item.index
+    //     );
+    //     // create in row1-> div2->class col-item.col
+    //     this.row = this.row - 1;
+    //     this.col = 12;
+    //     this.x[this.x.length - 1].row?.cols.push({
+    //       rowSize: item.rows,
+    //       colSize: item.cols,
+    //     });
+    //   } else if (ele.colSize - item.cols < 0) {
+    //     this.row = item.rows;
+    //     this.col = 12 - item.cols;
+    //     console.log(
+    //       'create row2&create div-class col-item.col',
+    //       item.cols,
+    //       item.index
+    //     );
+    //     // create row2&create div-class col-item.col
+    //   }
+    // }
   }
 
   getDom() {
